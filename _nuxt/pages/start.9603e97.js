13b75036025d22eb216ca7a5afcaa1223378648b
equaliser0 <accounts@equaliser.minimalcity.de>
2022-05-07 10:04:53 +0200

    update launch

--- _nuxt/pages/start.9603e97.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{377:function(e,t,n){"use strict";n.r(t);var c={},r=n(7),component=Object(r.a)(c,(function(){var e=this.$createElement;this._self._c;return this._m(0)}),[function(){var e=this.$createElement,t=this._self._c||e;return t("section",[t("div",{})])}],!1,null,"7c52eeb0",null);t.default=component.exports}}]);

3c3ddd9c90ce9fc108f31127e82f61d652a409ab
equaliser0 <accounts@equaliser.minimalcity.de>
2022-05-23 08:41:58 +0200

    extend launch

---
+++ _nuxt/pages/start.2dd668c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{1100:function(e,t,n){"use strict";n.r(t);var c={},r=n(9),component=Object(r.a)(c,(function(){var e=this.$createElement;this._self._c;return this._m(0)}),[function(){var e=this.$createElement,t=this._self._c||e;return t("section",[t("div",{})])}],!1,null,"7c52eeb0",null);t.default=component.exports}}]);
